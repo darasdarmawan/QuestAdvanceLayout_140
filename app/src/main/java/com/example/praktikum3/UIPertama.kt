4a5c6ebc6816a6ff2a57bba402939c20e20d780a
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-23 15:51:35 +0700

    Menambahkan fungsi composable ActivitasPertama

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -1,1 +1,16 @@
 package com.example.praktikum3
+
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+}
