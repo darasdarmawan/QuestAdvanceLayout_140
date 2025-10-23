23b0b7e8d3f6d8473ac00241d1100b522fbeafd2
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-23 16:17:30 +0700

    memperbaiki typo

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -1,14 +1,18 @@
 package com.example.praktikum3
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -36,6 +40,7 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                 .padding(12.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
+            )
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
@@ -81,7 +86,5 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                 )
 
             }
-        )
-
     }
 }
