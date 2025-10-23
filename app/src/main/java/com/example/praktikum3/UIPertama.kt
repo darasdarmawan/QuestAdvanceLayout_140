a193fcdbd4d951bc257c06e7a5e4c8a65458d612
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-23 15:53:59 +0700

    Menambahkan tampilan teks Prodi

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -1,9 +1,12 @@
 package com.example.praktikum3
 
 
-
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun ActivitasPertama(modifier: Modifier = Modifier) {
@@ -12,5 +15,12 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             .padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ) {
+        Text(
+            stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
 }
