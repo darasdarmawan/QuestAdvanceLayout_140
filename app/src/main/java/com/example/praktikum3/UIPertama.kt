abc95bd9d9f66b5396d325c259aab0c9b50bb4e7
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-23 15:54:56 +0700

    Menambahkan tampilan teks Univ

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -21,6 +21,11 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
+        Text(
+            stringResource(R.string.univ),
+            fontSize = 22.sp
+        )
+
 
     }
 }
