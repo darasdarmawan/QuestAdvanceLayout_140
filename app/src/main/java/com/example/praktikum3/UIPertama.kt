93160b0a705e6c0aa59e6369ac453eb6684e0e65
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-10-23 16:13:11 +0700

    Menambahkan teks copyright di bagian bawah

--- app/src/main/java/com/example/praktikum3/UIPertama.kt
+++ app/src/main/java/com/example/praktikum3/UIPertama.kt
@@ -72,7 +72,15 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-            )
+            ){
+                Text(
+                    stringResource(R.string.copy),
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .padding(bottom = 50.dp)
+                )
+
+            }
         )
 
     }
